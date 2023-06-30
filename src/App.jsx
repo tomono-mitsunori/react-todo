8335ff5b0ba1c5e04d984ed0cf99dbdd980c8c31
Mitsunori Tomono <134925130+tomono-mitsunori@users.noreply.github.com>
2023-06-30 09:21:09 +0000

    Update App.jsx and styles.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "ああああ",
+    "いいいい",
+  ]);
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      ;
-      <div>
-        <p>未完了のTODO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
         <ul>
-          <div>
-            <li>あああ</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
-          <div>
-            <li>いいい</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
-      ;
-      <div>
-        <p>完了のTODO</p>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>ううう</li>
-            <button>戻す</button>
-          </div>
-          <div>
-            <li>えええ</li>
             <button>戻す</button>
           </div>
         </ul>
